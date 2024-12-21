4126e758bde45817db1f0930040bc0cee81f7a82
Lewis O'Driscoll <lewis.odriscoll@gmail.com>
2024-12-21 20:33:30 +0000

    feat: day 5 part 1 solution

---
+++ internal/day05/main.go
@@ -0,0 +1,77 @@
+package day05
+
+import (
+	"github.com/lewis-od/aoc24/internal/common"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func Part1(input []string) int {
+	rules, pagesList := ParseInput(input)
+
+	result := 0
+	for _, pages := range pagesList {
+		if !ArePagesSorted(rules, pages) {
+			continue
+		}
+		middleValue := pages[len(pages)/2]
+		result += middleValue
+	}
+
+	return result
+}
+
+func ParseInput(input []string) (map[int][]int, [][]int) {
+	rules := make(map[int][]int)
+	pages := make([][]int, 0)
+
+	parsedAllRules := false
+	for _, line := range input {
+		if line == "" {
+			parsedAllRules = true
+			continue
+		}
+
+		if !parsedAllRules {
+			first, second := parseRule(line)
+			rules[first] = append(rules[first], second)
+		} else {
+			thesePages := parsePages(line)
+			pages = append(pages, thesePages)
+		}
+	}
+
+	return rules, pages
+}
+
+func parseRule(line string) (int, int) {
+	parts := strings.Split(line, "|")
+	if len(parts) != 2 {
+		panic("invalid rule: " + line)
+	}
+
+	first := common.Must(strconv.Atoi(parts[0]))
+	second := common.Must(strconv.Atoi(parts[1]))
+	return first, second
+}
+
+func parsePages(line string) []int {
+	parts := strings.Split(line, ",")
+	pages := make([]int, len(parts))
+	for n, part := range parts {
+		pages[n] = common.Must(strconv.Atoi(part))
+	}
+	return pages
+}
+
+func ArePagesSorted(rules map[int][]int, pages []int) bool {
+	compare := func(a, b int) int {
+		greater := rules[b]
+		if slices.Contains(greater, a) {
+			return 1
+		}
+		return -1
+	}
+	return slices.IsSortedFunc(pages, compare)
+}
---
+++ internal/day05/main_test.go
@@ -0,0 +1,101 @@
+package day05_test
+
+import (
+	"testing"
+
+	"github.com/lewis-od/aoc24/internal/day05"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPart1(t *testing.T) {
+	input := []string{
+		"47|53",
+		"97|13",
+		"97|61",
+		"97|47",
+		"75|29",
+		"61|13",
+		"75|53",
+		"29|13",
+		"97|29",
+		"53|29",
+		"61|53",
+		"97|53",
+		"61|29",
+		"47|13",
+		"75|47",
+		"97|75",
+		"47|61",
+		"75|61",
+		"47|29",
+		"75|13",
+		"53|13",
+		"",
+		"75,47,61,53,29",
+		"97,61,53,29,13",
+		"75,29,13",
+		"75,97,47,61,53",
+		"61,13,29",
+		"97,13,75,29,47",
+	}
+
+	result := day05.Part1(input)
+
+	require.Equal(t, 143, result)
+}
+
+func TestParseInput(t *testing.T) {
+	input := []string{
+		"75|13",
+		"53|13",
+		"53|20",
+		"",
+		"75,47,61,53,29",
+		"97,61,53,29,13",
+	}
+
+	rules, pages := day05.ParseInput(input)
+
+	expectedRules := map[int][]int{
+		75: {13},
+		53: {13, 20},
+	}
+	expectedPages := [][]int{
+		{75, 47, 61, 53, 29},
+		{97, 61, 53, 29, 13},
+	}
+	require.Equal(t, rules, expectedRules)
+	require.Equal(t, pages, expectedPages)
+}
+
+func TestArePagesSorted_Sorted(t *testing.T) {
+	rules := map[int][]int{
+		47: {53, 13, 61, 29},
+		97: {13, 61, 47, 29, 53, 75},
+		75: {29, 53, 47, 61, 13},
+		61: {13, 53, 29},
+		29: {13},
+		53: {29, 13},
+	}
+	pages := []int{75, 47, 61, 53, 29}
+
+	areSorted := day05.ArePagesSorted(rules, pages)
+
+	require.True(t, areSorted)
+}
+
+func TestArePagesSorted_NotSorted(t *testing.T) {
+	rules := map[int][]int{
+		47: {53, 13, 61, 29},
+		97: {13, 61, 47, 29, 53, 75},
+		75: {29, 53, 47, 61, 13},
+		61: {13, 53, 29},
+		29: {13},
+		53: {29, 13},
+	}
+	pages := []int{75, 97, 47, 61, 53}
+
+	areSorted := day05.ArePagesSorted(rules, pages)
+
+	require.False(t, areSorted)
+}
